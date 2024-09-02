3132866055ed4c9c8fa344f940bdc6a269caac51
YinkaiYu <yuyk6@mail2.sysu.edu.cn>
2024-09-02 16:27:36 +0800

    完善了U term演化部分

--- operator_Hubbard.f90
+++ operator_Hubbard.f90
@@ -11,33 +11,33 @@ module OperatorHubbard_mod
         complex(kind=8), public :: Delta
         complex(kind=8), public :: ratio_gaussian
     contains
-        procedure           :: set          => opK_set
-        procedure, private  :: get_exp      => opK_get_exp
-        procedure           :: get_delta    => opK_get_delta
-        procedure           :: mmult_R      => opK_mmult_R
-        procedure           :: mmult_L      => opK_mmult_L
+        procedure           :: set          => opU_set
+        procedure, private  :: get_exp      => opU_get_exp
+        procedure           :: get_delta    => opU_get_delta
+        procedure           :: mmult_R      => opU_mmult_R
+        procedure           :: mmult_L      => opU_mmult_L
     end type OperatorHubbard
     
 contains
-    subroutine opK_set(this, RU)
+    subroutine opU_set(this, RU)
         class(OperatorHubbard), intent(inout) :: this
         real(kind=8), intent(in) :: RU
         this%alpha = dcmplx( 0.d0, 0.d0 )
         if ( RU < -Zero ) this%alpha = dcmplx( sqrt(-2.d0 * RU * Dtau), 0.d0 )
         if ( RU >  Zero ) this%alpha = dcmplx( 0.d0, sqrt( 2.d0 * RU * Dtau) )
         return
-    end subroutine opK_set
+    end subroutine opU_set
     
-    subroutine opK_get_exp(this, phi, nflag)
+    subroutine opU_get_exp(this, phi, nflag)
         class(OperatorHubbard), intent(inout) :: this
         integer, intent(in) :: nflag ! +1 or -1; propagating direction
         real(kind=8), intent(in) :: phi ! space time local auxiliary field value, for phi_1 or phi_2
         this%gaussian = dcmplx( exp(-0.5d0 * phi * phi), 0.d0 )
         this%expalpha = exp( this%alpha * phi * nflag )
         return
-    end subroutine opK_get_exp
+    end subroutine opU_get_exp
     
-    subroutine opK_get_delta(this, phi_old, phi_new)
+    subroutine opU_get_delta(this, phi_old, phi_new)
         class(OperatorHubbard), intent(inout) :: this
         real(kind=8), intent(in) :: phi_old, phi_new
         complex(kind=8) :: gaussian_old, gaussian_new
@@ -51,70 +51,39 @@ contains
         this%ratio_gaussian = gaussian_new / gaussian_old
         this%Delta          = expalpha_new / expalpha_old - dcmplx(1.d0,0.d0)
         return
-    end subroutine opK_get_delta
+    end subroutine opU_get_delta
     
-    subroutine opK_mmult_R(this, Mat, Latt, phi, ntau, nflag)
+    subroutine opU_mmult_R(this, Mat, Latt, phi, ii, nflag)
 ! Arguments: 
         class(OperatorHubbard), intent(inout) :: this
         complex(kind=8), dimension(Ndim, Ndim), intent(inout) :: Mat
         class(kagomeLattice), intent(in) :: Latt
-        real(kind=8), dimension(Naux, Lq, Ltrot), intent(in) :: phi
-        integer, intent(in) :: ntau, nflag
+        real(kind=8), intent(in) :: phi
+        integer, intent(in) :: ii, nflag
 ! Local: 
-        integer :: P(Norb), ii, no, j, sign
-        real(kind=8), dimension(Naux) :: vec
-        complex(kind=8), dimension(2, Ndim) :: Vhlp
+        integer :: jj
 
-        do ii = 1, Lq
-            vec(:) = phi(:, ii, ntau)
-            call this%get_exp(vec, nflag) ! output entryC and entryS
-            sign = 1
-            do no = 1, Norb
-                P(no) = Latt%inv_dim_list(ii, no)
-            enddo
-            Vhlp = dcmplx(0.d0, 0.d0)
-            do j = 1, Ndim
-                Vhlp(1, j) = this%entryC * Mat(P(1), j) + sign * this%entryS * Mat(P(2), j)
-                Vhlp(2, j) = sign * dconjg(this%entryS) * Mat(P(1), j) + this%entryC * Mat(P(2), j)
-            enddo
-            do j = 1, Ndim
-                Mat(P(1), j) = Vhlp(1, j)
-                Mat(P(2), j) = Vhlp(2, j)
-            enddo
+        call this%get_exp(phi, nflag)
+        do jj = 1, Ndim
+            Mat(ii,jj) = this%expalpha * Mat(ii,jj)
         enddo
         return
-    end subroutine opK_mmult_R
+    end subroutine opU_mmult_R
     
-    subroutine opK_mmult_L(this, Mat, Latt, phi, ntau, nflag) 
-! In Mat Out Mat* EXP(D(NF)) * UT(NF)
-! Arguments:
+    subroutine opU_mmult_L(this, Mat, Latt, phi, jj, nflag)
+! Arguments: 
         class(OperatorHubbard), intent(inout) :: this
         complex(kind=8), dimension(Ndim, Ndim), intent(inout) :: Mat
         class(kagomeLattice), intent(in) :: Latt
-        real(kind=8), dimension(Naux, Lq, Ltrot), intent(in) :: phi
-        integer, intent(in) :: ntau, nflag
+        real(kind=8), intent(in) :: phi
+        integer, intent(in) :: jj, nflag
 ! Local: 
-        integer :: P(Norb), j, ii, no, sign
-        real(kind=8), dimension(Naux) :: vec
-        complex(kind=8), dimension(Ndim, 2) :: Uhlp
+        integer :: ii
 
-        do ii = 1, Lq
-            vec(:) = phi(:, ii, ntau)
-            call this%get_exp(vec, nflag) ! output entryC and entryS
-            sign = 1
-            do no = 1, Norb
-                P(no) = Latt%inv_dim_list(ii, no)
-            enddo
-            Uhlp = dcmplx(0.d0, 0.d0)
-            do j = 1, Ndim
-                Uhlp(j, 1) = Mat(j, P(1)) * this%entryC + sign * Mat(j, P(2)) * dconjg(this%entryS)
-                Uhlp(j, 2) = sign * Mat(j, P(1)) * this%entryS + Mat(j, P(2)) * this%entryC
-            enddo
-            do j = 1, Ndim
-                Mat(j, P(1)) = Uhlp(j, 1)
-                Mat(j, P(2)) = Uhlp(j, 2)
-            enddo
+        call this%get_exp(phi, nflag)
+        do ii = 1, Ndim
+            Mat(ii,jj) = Mat(ii,jj) * this%expalpha
         enddo
         return
-    end subroutine opK_mmult_L
+    end subroutine opU_mmult_L
 end module OperatorHubbard_mod
